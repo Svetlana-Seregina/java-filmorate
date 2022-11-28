099dfefc812b80229b50f6ce00d4848a4259b84c
jacksonDenisov <jackson.denisov@yandex.ru>
2022-11-29 01:08:01 +0300

    fix: привести к единому виду логи контроллеров

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -23,18 +23,19 @@ public class FilmController {
 
     @GetMapping("/{id}")
     public Film findFilmById(@PathVariable Long id) {
+        log.info("Обрабатываем запрос на получение фильма с id = " + id);
         return filmService.getFilmById(id);
     }
 
     @PostMapping
     public Film createFilm(@Valid @RequestBody Film film) {
-        log.debug("Обрабатываем запрос на создание фильма " + film);
+        log.info("Обрабатываем запрос на создание фильма " + film);
         return filmService.createFilm(film);
     }
 
     @GetMapping
     public List<Film> findAll() {
-        log.debug("Текущее количество фильмов: {}", filmService.findAll().size());
+        log.info("Обрабатываем запрос на получение списка всех фильмов");
         return filmService.findAll();
     }
 
@@ -46,28 +47,34 @@ public class FilmController {
              @RequestParam(value = "genreId", required = false) Long genreId,
              @RequestParam (value = "year", required = false) Integer year) {
 
-        log.info("Получен запрос к эндпоинту на получение фильмов: count = {}, жанр id = {}, год = {} "
-                , count, genreId, year);
+        log.info("Обрабатываем запрос на получение фильмов: count = {}, жанр id = {}, год = {} ",
+                count, genreId, year);
         return likeService.findPopularsFilmsByGenreOrAndYear(count, genreId, year);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film film) {
+        log.info("Обрабатываем запрос на обновление фильма " + film);
         return filmService.updateFilm(film);
     }
 
     @PutMapping("/{id}/like/{userId}")
     public boolean addLikeToFilm(@PathVariable Long id, @PathVariable Long userId) {
+        log.info("Обрабатываем запрос на добавление лайка фильму с id = {} от пользователя с id = {}",
+                id, userId);
         return likeService.addLikeToFilm(id, userId);
     }
 
     @DeleteMapping("/{id}/like/{userId}")
     public void deleteLikeFromFilm(@PathVariable Long id, @PathVariable Long userId) {
+        log.info("Обрабатываем запрос на удаление лайка у фильма с id = {} от пользователя с id = {}",
+                id, userId);
         likeService.removeLikeFromFilm(id, userId);
     }
 
     @DeleteMapping("/{id}")
     public boolean deleteFilm(@PathVariable Long id) {
+        log.info("Обрабатываем запрос на удаление фильма с id = " + id);
         return filmService.deleteFilm(id);
     }
 
@@ -76,19 +83,21 @@ public class FilmController {
     @GetMapping("/search")
     public List<Film> searchFilmsBySubstring(@RequestParam(value = "query") String query,
                                   @RequestParam(value = "by", required = false, defaultValue = "title") String by) {
+        log.info("Обрабатываем запрос на поиск фильма по строке: " + query);
         return filmService.searchFilmsBySubstring(query, by);
     }
 
     @GetMapping("/director/{directorId}")
     public List<Film> getFilmsByDirector(@PathVariable Long directorId, @RequestParam String sortBy) {
-        log.debug("Обрабатываем запрос на получение списка фильмов режиссера с id " + directorId +
-                " отсортированных по признаку " + sortBy);
+        log.info("Обрабатываем запрос на получение списка фильмов режиссера с id = {}, отсортированных по признаку {}",
+                directorId ,sortBy);
         return filmService.getFilmsByDirector(directorId, sortBy);
     }
 
     @GetMapping("/common")
     public List<Film> getCommonFilms(@RequestParam Long userId, @RequestParam Long friendId) {
-        log.debug("Обрабатываем запрос на получение общих фильмов для пользователей " + userId + " и " + friendId);
+        log.info("Обрабатываем запрос на получение общих фильмов для пользователей с id = {} и id = {}",
+                userId, friendId);
         return filmService.getCommonFilms(userId, friendId);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Mpa;
 import ru.yandex.practicum.filmorate.service.MpaService;
+
 import java.util.List;
 
 @RequestMapping("/mpa")
@@ -17,13 +18,13 @@ public class MpaController {
 
     @GetMapping("/{id}")
     public Mpa findMpaById(@PathVariable Long id) {
-        log.info("Получен запрос на получение mpa_id = {}", id);
+        log.info("Обрабатываем запрос на получение рейтинга MPA с id = " + id);
         return mpaService.findMpaById(id);
     }
 
     @GetMapping
     public List<Mpa> findAllMpa() {
+        log.info("Обрабатываем запрос на получение списка всех рейтингов MPA");
         return mpaService.findAllMpa();
     }
-
 }
